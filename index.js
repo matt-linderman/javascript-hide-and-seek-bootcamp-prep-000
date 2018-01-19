9c7c72a230d8480d1b41c2e111c0336001c70594
Matthew Linderman <mwl53@mit.edu>
2018-01-19 20:44:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,21 +7,22 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var rankList = document.querySelectorAll(".ranked-list")
+  var rankList = document.querySelectorAll(".ranked-list");
   
   for (var i = 0; i < rankList.length; i++) {
-    rankList[i].innerHTML = parseInt(rankList[i].innerHTML) + parseInt(n)
+    rankList[i].innerHTML = parseInt(rankList[i].innerHTML) + parseInt(n);
   }
 }
 
 function deepestChild() {
   var grandNodeList = document.querySelectorAll("#grand-node");
-  for (var i = 0; i < grandNodeList.length; i++) {
-    if (grandNodeList[i].child === undefined) {
-      return grandNodeList[i]
+  var go = 1;
+  
+  while (go === 1) {
+    if (grandNodeList.child === undefined) {
+      go = 0;
     } else {
-      grandNodeList = grandNodeList[i].child
+      grandNodeList = grandNodeList.child;
     }
-    var layer = grandNodeList[i].child 
   }
 }
